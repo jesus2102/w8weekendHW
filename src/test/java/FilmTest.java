162e9d293d6fae656142c6879a7c289709d3376b
jesus2102 <jesusperezmarin@gmail.com>
2018-07-13 12:56:28 +0100

    Test Film canSetTitle passed

--- src/test/java/FilmTest.java
+++ src/test/java/FilmTest.java
@@ -9,16 +9,20 @@ import static org.junit.Assert.assertEquals;
 public class FilmTest {
 
     Studio studio1;
+    Studio studio2;
     Film film1;
     Film film2;
     Director director1;
+    Director director2;
 
 
     @Before
     public void before(){
         studio1 = new Studio("Walt Disney");
+        studio2 = new Studio("MGM");
 
         director1 = new Director("James", "Cameron");
+        director1 = new Director("Peter", "Jackson");
 
         film1 = new Film("Avengers", studio1, director1);
         film2 = new Film("IronMan", studio1, director1);
@@ -28,4 +32,12 @@ public class FilmTest {
     public void hasTitle(){
         assertEquals("Avengers", film1.getTitle());
     }
+
+    @Test
+    public void canSetTitle(){
+        film1.setTitle("Avengers 2");
+        assertEquals("Avengers 2", film1.getTitle());
+    }
+
+
 }
